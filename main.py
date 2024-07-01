604a2e0936b9b4d960dbc9fdc80c6cf6a20d8990
m4549071758 <m4549071758@gmail.com>
2024-07-01 18:01:12 +0900

    構造変更

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from mcstatus import JavaServer
+from datetime import datetime
 import discord
 import asyncio
 
 ADDRESS = "pechi.jp"
 PORT = 25565
-TOKEN = "YOUR_DISCORD_TOKEN"
+TOKEN = "YOUR_TOKEN"
 CHANNEL_ID = 1257251108787589161
+VERSION = "1.12.x - 1.21.x"
+status = ""
 
 def server_info(ADDRESS, PORT):
     server = JavaServer(ADDRESS, PORT)
 
     status = server.status()
 
-    version = status.version.name
     ping = status.latency
     players_online = status.players.online
     players_max = status.players.max
 
-    return round(ping, 2), players_online, players_max, version
+    return round(ping, 2), players_online, players_max
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -27,29 +29,31 @@ async def on_ready():
     print("------")
 
     # 10分ごとに、Minecraft サーバーの情報を取得しチャンネルにメッセージを送信する。
-    # メッセージを送信する場合、チャンネルに1つ以上メッセージが存在する場合は、そのメッセージを編集する形式にする。
     while True:
         try:
-            ping, players_online, players_max, version = server_info(ADDRESS, PORT)
-            channel = client.get_channel(CHANNEL_ID)
-            messages = await channel.history(limit=1).flatten()
-            if len(messages) == 0:
-                await channel.send(f"Minecraft サーバー {ADDRESS} の情報:")
-                await channel.send(f"バージョン: {version}")
-                await channel.send(f"Ping: {ping}ms")
-                await channel.send(f"オンラインプレイヤー数: {players_online}/{players_max}")
-            else:
-                await messages[0].edit(content=f"Minecraft サーバー {ADDRESS} の情報:")
-                await messages[1].edit(content=f"バージョン: {version}")
-                await messages[2].edit(content=f"Ping: {ping}ms")
-                await messages[3].edit(content=f"オンラインプレイヤー数: {players_online}/{players_max}")
-        except Exception:
-            channel = client.get_channel(CHANNEL_ID)
-            messages = await channel.history(limit=1).flatten()
-            if len(messages) == 0:
-                await channel.send("サーバーはオフラインです")
-            else:
-                await messages[0].edit(content="サーバーはオフラインです")
+            ping, players_online, players_max = server_info(ADDRESS, PORT)
+            status = "オンライン"
+        except:
+            ping, players_online, players_max = "NaN ", "0 ", " 0"
+            status = "オフライン"
+        channel = client.get_channel(CHANNEL_ID)
+
+        # チャンネルにメッセージが存在する場合、すべて削除する
+        async for message in channel.history(limit=2):
+            await message.delete()
+        
+        # メッセージを送信する
+        await channel.send("```yaml\n"
+                            f"サーバー: {ADDRESS}\n"
+                            f"ステータス: {status}\n"
+                            f"バージョン: {VERSION}\n"
+                            f"プレイヤー: {players_online}/{players_max}\n"
+                            f"ping: {ping}ms\n"
+                            f"最終更新: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}\n"
+                            "```")
+        
+        print(f"status: {status} | players: {players_online}/{players_max} | ping: {ping}ms | last update: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
         await asyncio.sleep(600)
+
 
 client.run(TOKEN)
